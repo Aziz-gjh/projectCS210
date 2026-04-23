1a6d9889c8a0d9a7fbe70de1995b63a201034cb0
Doody06 <Abdelrahmanschool06@gmail.com>
2026-04-23 12:57:33 +0300

    Worked on NEOM_Core basic logic

---
+++ NEOM_Core.java
@@ -0,0 +1,67 @@
+public class NEOM_Core {
+    AVLTree sectors; 
+    TaskQueue deployment;
+    ActionStack undoLog;
+    ArchiveList history;
+
+    public NEOM_Core() {
+        sectors = new AVLTree();
+        deployment = new TaskQueue();
+        undoLog = new ActionStack();
+        history = new ArchiveList();
+    }
+    
+    public void addTask(int sectorID, String taskID, String desc) {
+        //insert into AVL tree
+        sectors.Insert(sectorID);
+        //insert into Queue
+        TaskNode newTask = new TaskNode(sectorID, taskID, desc);
+        deployment.Enqueue(newTask);
+        //push onto the stack
+        undoLog.Push(newTask);
+        
+    }
+
+    public void processNextTask() {
+        //dequeue next task
+        TaskNode nextTask = deployment.Dequeue(); 
+        //update its status in the AVL Tree to "Completed"
+        sectors.completeTask(nextTask);
+        //Insert it at the tail of the ArchiveList
+        history.Append(nextTask);
+    }
+
+    public void undoLastAction() {
+        //Pop the stack
+        TaskNode removedTask = undoLog.Pop();
+        //Remove the popped task from AVL tree and Queue \
+        sectors.Remove(removedTask)
+        deployment.Dequeue(removedTask) //WE MUST ASK THE DR. ABOUT THIS
+
+        //extra: Update log for undos as well
+    }
+
+    public void systemAudit() {
+        //Perform LVR traversal of AVL tree and print all sectors and their tasks
+        sectors.Traverse() //I'll implement the printing inside the Traverse
+    }
+
+    public void printDeploymentHistory() {
+        //Traverse ARchiveList from head to tail and print all tasks in order
+        history.Traverse() //Abdulaziz will implement the printing in the Traverse
+    }
+
+    public void searchSector(int sectorID) {
+        //Search the AVL tree for sectorID
+        TasnkNode SearchedTask = sectors.Search(sectorID)
+        //print number of comparisons vs. total Sectors (demonstrate that it is O(log n))
+        //WIP HERE
+
+    }
+    @Override
+    public String toString() {
+        return "";
+    } 
+    
+}
+}
---
+++ Solution.java
@@ -0,0 +1,100 @@
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        NEOM_Core system = new NEOM_Core(); // Should be an error because we didn't write NEOM_CORE yet
+
+        while (true) {
+            try {
+                int command = sc.nextInt();
+
+                switch (command) {
+
+                    //<filename>  
+                    case 1:
+                        String filename = sc.next();
+                        loadTasksFromFile(filename, system); //This method loads tasks AND calls addTask for each one of them.
+                        break;
+
+                    //Process Next Task
+                    case 2:
+                        system.processNextTask();
+                        break;
+
+                    //Emergency Undo
+                    case 3:
+                        system.undoLastAction();
+                        break;
+
+                    //System Audit
+                    case 4:
+                        system.systemAudit();
+                        break;
+
+                    //View History
+                    case 5:
+                        system.printDeploymentHistory();
+                        break;
+
+                    //<sectorID> → Search Sector
+                    case 6:
+                        int sectorID = sc.nextInt();
+                        system.searchSector(sectorID);
+                        break;
+
+                    default:
+                        System.out.println("Invalid command.");
+
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error reading input.");
+                break;
+            }
+        }
+
+        sc.close();
+    }
+
+    private static void loadTasksFromFile(String filename, NEOM_Core system) {
+        BufferedReader br = null; //BufferedReader's advantage is that it can do readLine()
+        try {
+            br = new BufferedReader(new FileReader(filename));
+            String line = "";
+
+            while ((line = br.readLine()) != null) {
+
+                if (line.trim().isEmpty()) continue;
+
+                // Format: sectorID, taskID, description
+                String[] parts = line.split(",");
+
+                if (parts.length < 3) continue;
+                else System.out.println("Invalid file format."); 
+
+                int sectorID = Integer.parseInt(parts[0].trim());
+                String taskID = parts[1].trim();
+                String desc = parts[2].trim();
+
+                system.addTask(sectorID, taskID, desc); //we call Neom_Core here
+            }
+
+        } catch (IOException e) {
+            System.out.println("File error: " + filename);
+        } catch (Exception e) {
+            System.out.println("Invalid file format.");
+        } finally { //I read that this block is not needed in new Java versions but kept it just in case.
+            try {
+                if (br != null) br.close();
+            } catch (IOException e) {
+                System.out.println("Error closing file.");
+            }
+        }
+    }
+}
